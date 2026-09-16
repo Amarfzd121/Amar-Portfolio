2f7d0c48a02f64d8ede625a6dee5e4d9f6292e9f
AMAR SINGH <100669788+Amarfzd121@users.noreply.github.com>
2026-09-17 00:08:29 +0530

    Implement CORS support in ContactFormHandler
    Add CORS headers and OPTIONS method to handler

--- server.py
+++ server.py
@@ -8,6 +8,16 @@ PORT = 8001
 MESSAGES_FILE = "messages.txt"
 
 class ContactFormHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        super().end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.end_headers()
+
     def do_POST(self):
         if self.path == '/submit':
             # Parse the form data
